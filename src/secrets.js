eec8d66a279768de7826b98111c59726e06a2bfe
Tal Ben David <talbd51@gmail.com>
2022-03-03 11:22:36 +0200

    2

--- src/secrets.js
+++
@@ -1,3 +0,0 @@
-export const googleMapApiKey = 'AIzaSyA9R0O6Id3TI8oepEEAlNg0a--GNN_QtcE'
-export const cloudinaryAPIKey = '646791858889346'
-export const cloudinaryAPISecret = 'jZi3D6ip5m5-jyrB8vZjx7EXWVQ'
