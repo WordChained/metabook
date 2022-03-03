6239093b4a75cddaffc28e1bfc73f5d9ba520808
Tal Ben David <talbd51@gmail.com>
2022-03-03 11:19:11 +0200

    adding secrets

---
+++ src/secrets.js
@@ -0,0 +1,3 @@
+export const googleMapApiKey = 'AIzaSyA9R0O6Id3TI8oepEEAlNg0a--GNN_QtcE'
+export const cloudinaryAPIKey = '646791858889346'
+export const cloudinaryAPISecret = 'jZi3D6ip5m5-jyrB8vZjx7EXWVQ'
