8858a2446eb23805603a96f1da5e521156c10670
Tal Ben David <talbd51@gmail.com>
2022-04-21 11:34:02 +0300

    went back to previous commit

--- src/services/unsplash-service.js
+++ src/services/unsplash-service.js
@@ -1,32 +1,31 @@
-// import { UNSPLASH_ACCESS_KEY } from "../secrets";
+import { secrets } from "../secrets";
+export const unsplashService = {
+    searchUnsplash,
+    fetchResults
+}
 
-// export const unsplashService = {
-//     searchUnsplash,
-//     fetchResults
-// }
+async function searchUnsplash(searchQuery) {
+    let sec
+    try {
+        sec = await secrets
+    } catch (err) {
+        console.log("couldnt grab secrets");
+    }
+    const endpoint = `https://api.unsplash.com/search/photos?query=${searchQuery}&client_id=${sec.UNSPLASH_ACCESS_KEY}`;
+    const response = await fetch(endpoint);
+    if (!response.ok) {
+        throw Error(response.statusText);
+    }
+    const json = await response.json();
+    return json;
+}
 
-// async function searchUnsplash(searchQuery) {
-//     let sec
-//     try {
-//         sec = await secrets
-//     } catch (err) {
-//         console.log("couldnt grab secrets");
-//     }
-//     const endpoint = `https://api.unsplash.com/search/photos?query=${searchQuery}&client_id=${UNSPLASH_ACCESS_KEY}`;
-//     const response = await fetch(endpoint);
-//     if (!response.ok) {
-//         throw Error(response.statusText);
-//     }
-//     const json = await response.json();
-//     return json;
-// }
-
-// async function fetchResults(searchQuery) {
-//     try {
-//         const results = await searchUnsplash(searchQuery);
-//         return results;
-//     } catch (err) {
-//         console.log(err);
-//         console.log('Failed to search Unsplash');
-//     }
-// }
+async function fetchResults(searchQuery) {
+    try {
+        const results = await searchUnsplash(searchQuery);
+        return results;
+    } catch (err) {
+        console.log(err);
+        console.log('Failed to search Unsplash');
+    }
+}
